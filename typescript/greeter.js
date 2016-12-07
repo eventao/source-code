20848f5daae89072accd7467faa0798e7f4fdb98
eventao <330293577@qq.com>
2016-12-07 17:59:32 +0800

    数组操作

---
+++ typescript/greeter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yuant on 2016/12/7.
+ */
+function greeter(person) {
+    return "Hello," + person;
+}
+// var user = "Jane User";
+var user = [0, 1, 2];
+document.body.innerHTML = greeter(user);
