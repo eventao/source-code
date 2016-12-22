4185b026b57cee3bb848af5615f178b391a6dd10
eventao <330293577@qq.com>
2016-12-22 11:22:20 +0800

    Calendar

---
+++ scripts-practice/calendar.js
@@ -0,0 +1,11 @@
+/**
+ * Created by forli on 2016/12/21.
+ */
+// function Calander(){
+//     var now = new Date();
+//     var month = now.getMonth() + 1;
+//     var date = now.getDate();
+//     var year = now.getFullYear();
+//     var day = now.getDay();
+//     sds
+// }
