e4166a0f3e670520694d02d3fac3a6b7681f77e1
eventao <330293577@qq.com>
2016-12-24 23:31:19 +0800

    路由机制

--- server/router/index.js
+++ server/router/index.js
@@ -3,19 +3,16 @@
  */
 
 var http = require("http");
-function running(route){
-    var port = 8888;
-    http.createServer(function(request,response){
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received!");
-        route(pathname);
-        response.writeHead(200,{"Content-Type":"text/plain"});
-        response.write("Hello World");
-        response.end();
-    }).listen(port);
-    console.log("running on " + port);
-}
-running(function route(pathname) {
-    console.log("About to route a request for " + pathname);
+
+var port = 8888;
+var server = http.createServer(function(request,response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received!");
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("Hello World");
+    response.end();
 });
+server.listen(port);
+console.log("running on " + port);
+
 
