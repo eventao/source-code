fb1af1b3dd9dde7942491c21caf0662aee20fa5c
eventao <330293577@qq.com>
2016-12-24 22:11:20 +0800

    路由机制

--- server/router/index.js
+++ server/router/index.js
@@ -1,19 +1,21 @@
 /**
  * Created by forli on 2016/12/24.
  */
-var http = require('http');
-var url = require('url');
 
-function start(){
-    function onRequest(request,response){
+var http = require("http");
+function running(route){
+    var port = 8888;
+    http.createServer(function(request,response){
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received!");
+        route(pathname);
         response.writeHead(200,{"Content-Type":"text/plain"});
         response.write("Hello World");
         response.end();
-    }
-    var port = 8888;
-    http.createServer(onRequest).listen(port);
+    }).listen(port);
     console.log("running on " + port);
 }
-exports.start = start;
+running(function route(pathname) {
+    console.log("About to route a request for " + pathname);
+});
+
