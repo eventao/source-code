0e45438a41378463d217f5f1f06fe2c9568e9efb
eventao <330293577@qq.com>
2016-12-26 19:03:20 +0800

    路由机制

--- run.js
+++ run.js
@@ -1,10 +1,17 @@
 /**
  * Created by yuant on 2016/12/6.
  */
-
 var express = require('express');
 var app = express();
 app.use('/',express.static(__dirname));
+var router = express.Router();
+router.post("/menu/list",function(req,res){
+    req.on('data',function(data){
+        var dataR = decodeURIComponent(data);
+        res.json(dataR);
+    });
+});
+app.use('/',router);
 
 var port = 8888;
 app.listen(port);
