28b1ed1a96dba80c753066439967f4cc657932db
eventao <330293577@qq.com>
2017-01-18 20:56:51 +0800

    angular

--- angularjs/script.js
+++ angularjs/script.js
@@ -1,51 +1,74 @@
-/**
- * Created by forli on 2017/1/2.
- */
+(function (ag, window) {
+    var main = ag.module("mainModel", []);
+    
+    main.controller("mainController",
+        ["$scope", "$interval","$rootScope"
+            , function ($scope, $interval,$rootScope) {
 
-(function(window){
-    window.angular = {
-        modules:{}
-    };
+            $scope.containerStyle = {
+                "background-color":"blue",
+                width:"100px",
+                height:"80px"
+            };
+            $scope.classContainer = "container";
 
-    /**
-     * 确定程序边界
-     * @type {NodeList}
-     */
-    var html = document.getElementsByTagName('html');
-    var otterElement,dataModel;
-    (function findApp(eles,callBack){
-        for(var i = 0; i < eles.length; i++){
-            var ngApp = eles[i].getAttribute("ng-app");
-            if(ngApp){
-                callBack(eles[i],ngApp);
-                break;
-            }else{
-                var children = eles[i].children;
-                if(children.length){
-                    findApp(children,callBack);
+            
+            $scope.$on("emit-test",function(event,m){
+                alert(m);
+            });
+
+            $scope.title = "sdfsdf";
+            $scope.obj = {
+                name:"I'm mgModel + 花括号"
+            };
+            $scope.$watch("output",function(){
+                $scope.$broadcast("broad-test","broad-message");
+            });
+            $rootScope.basisMsg = "I'm root";
+
+            $scope.output = "";
+            $scope.now = "";
+            $interval(function () {
+                var nn = new Date();
+                $scope.now = nn.getMinutes()
+                    + "," + nn.getSeconds();
+            }, 1000);
+            $scope.students = ["陈益民", "张杨羊", "曾俊杰"];
+            $scope.teacher = [
+                {
+                    name: "戴老师",
+                    gender: "男",
+                    occupation: "CEO",
+                    special: "帅"
                 }
-            }
-        }
-    })(html,function(modelEle,model){
-            otterElement = modelEle;
-            dataModel = model;
-        });
+                , {
+                    name: "周老师",
+                    gender: "男",
+                    occupation: "team-leader",
+                    special: "cute"
+                }
+                , {
+                    name: "向老师",
+                    gender: "女",
+                    occupation: "manager",
+                    special: "beautiful"
+                }
+            ];
 
+        }]);
 
-    if(otterElement){
-    }
+    main.controller("childController",
+        ["$scope","$rootScope",function ($scope,$rootScope) {
+            // var p = $scope.$parent;
+            // alert($scope.teacher.length);
+            // alert($rootScope.basisMsg);
 
-    if(dataModel){
-        angular.modules[dataModel] = {
+            $scope.$on("broad-test",function(event,param){
+                alert(param);
+            });
 
-        };
-    }
+            $scope.$emit("emit-test","emit-message!");
 
-    if(!otterElement || !dataModel){
-        return;
-    }
+        }]);
 
-
-
-
-})(window);
+})(angular, window);
