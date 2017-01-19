2d1f456074c305bf97fde0bb3dbab02e676e54cf
eventao <330293577@qq.com>
2017-01-19 10:57:58 +0800

    angular

--- angularjs/script.js
+++ angularjs/script.js
@@ -1,9 +1,102 @@
 (function (ag, window) {
     var main = ag.module("mainModel", []);
-    
+
+    main.factory("DataFactory"
+        ,[function(){
+            //此处省略ajax请求
+            return {
+                flag:1,
+                msg:"请求成功",
+                code:200,
+                content:[
+                    {
+                        uuid:"addf34-dfg2-bdfg3-dfef2-fdf",
+                        occupation:"董事长",
+                        name:"王健林",
+                        pId:""
+                    }
+
+                    ,{
+                        uuid:"sldjfw-ewf-fewfwe-fewfbf",
+                        occupation:"财务部经理",
+                        name:"王思聪",
+                        pId:"addf34-dfg2-bdfg3-dfef2-fdf"
+                    }
+                    ,{
+                        uuid:"dskf-32ee-e23sdf-few",
+                        occupation:"人事部经理",
+                        name:"司马懿",
+                        pId:"addf34-dfg2-bdfg3-dfef2-fdf"
+                    }
+                    ,{
+                        uuid:"sdf-fdsfds-f2eff-sdd",
+                        occupation:"技术部经理",
+                        name:"吕布",
+                        pId:"addf34-dfg2-bdfg3-dfef2-fdf"
+                    }
+                    ,{
+                        uuid:"sdsd-fewf23-f23e23e",
+                        occupation:"销售部经理",
+                        name:"孙尚香",
+                        pId:"addf34-dfg2-bdfg3-dfef2-fdf"
+                    }
+
+                    ,{
+                        uuid:"sdfsdf-dsfdsff-23e2e",
+                        name:"赵云",
+                        occupation:"employer",
+                        pId:"sldjfw-ewf-fewfwe-fewfbf"
+                    }
+                    ,{
+                        uuid:"sldf-3e3dsf-r33e23e",
+                        name:"蒋干",
+                        occupation:"employer",
+                        pId:"dskf-32ee-e23sdf-few"
+                    }
+                    ,{
+                        uuid:"sdfsd-fe32e-e23ee3",
+                        name:"张飞",
+                        occupation:"employer",
+                        pId:"sdf-fdsfds-f2eff-sdd"
+                    }
+                    ,{
+                        uuid:"sdlf-32e2e-dfdfsf3",
+                        name:"杨玉环",
+                        occupation:"employer",
+                        pId:"sdsd-fewf23-f23e23e"
+                    }
+                ],
+                getRoots:function(){
+                    var result = [];
+                    this.content.forEach(function(d){
+                        if(d.pId === ""){
+                            result.push(d);
+                        }
+                    });
+                    return result;
+                },
+                getNodeByPid:function(pid){
+                    var result = [];
+                    this.content.forEach(function(d){
+                        if(d.pId === pid){
+                            result.push(d);
+                        }
+                    });
+                    return result;
+                }
+            };
+        }]);
+
     main.controller("mainController",
-        ["$scope", "$interval","$rootScope"
-            , function ($scope, $interval,$rootScope) {
+        ["$scope", "$interval","$rootScope","DataFactory"
+            , function ($scope, $interval,$rootScope,service) {
+            $scope.roots = service.getRoots();
+            $scope.roots.forEach(function(root){
+                root.children = service.getNodeByPid(root.uuid);
+                root.children.forEach(function(child){
+                    child.grandSons = service.getNodeByPid(child.uuid);
+                });
+            });
 
             $scope.containerStyle = {
                 "background-color":"blue",
